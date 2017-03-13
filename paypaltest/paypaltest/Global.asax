ea82e5615d4cc06115aff2a6a148a62f20b4b09d
buttersnipps <sbarua91@gmail.com>
2017-03-13 02:32:35 -0400

    Paypal using REST_API
    A paypal app on ASP.Net

---
+++ paypaltest/paypaltest/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="paypaltest.MvcApplication" Language="C#" %>
---
+++ paypaltest/paypaltest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(paypaltest.Startup))]
+namespace paypaltest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
